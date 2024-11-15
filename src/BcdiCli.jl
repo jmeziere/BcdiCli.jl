b9507f7bf4a2ad7056464d666acc1056dd834c75
jmeziere <jmeziere@gmail.com>
2024-11-15 11:30:23 -0700

    init

--- src/BcdiCli.jl
+++ src/BcdiCli.jl
@@ -1,5 +1,20 @@
 module BcdiCli
+    using ReplMaker
+    using REPL
+    using InteractiveUtils
 
-# Write your package code here.
+    function parse_to_expr(s)
+        quote Meta.parse($s) end
+    end
 
+println("here")
+    initrepl(
+        parse_to_expr,
+        prompt_text="BcdiCli> ",
+        prompt_color = :red,
+        start_key='>',
+        mode_name="Bcdi_mode",
+        valid_input_checker=complete_julia
+    )
+println("here")
 end
